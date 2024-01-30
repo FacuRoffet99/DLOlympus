eeac6bab9aaa578c3dd970f33913b2324556c343
FacuRoffet99 <89995713+FacuRoffet99@users.noreply.github.com>
2024-01-30 13:10:40 -0300

    Update mlflow.py

--- training/mlflow.py
+++ training/mlflow.py
@@ -25,19 +25,25 @@ def mlflow_train(learn, hyperparameters, callbacks, metrics_names, path, experim
         learn.fine_tune(hyperparameters['EPOCHS'], base_lr=hyperparameters['LR'], cbs=callbacks)
 
         # Create and log figures
-        mlflow.log_figure(plot_losses(learn), 'losses.png', save_kwargs={'bbox_inches': 'tight'})
-        mlflow.log_figure(plot_metrics(learn, metrics_names), 'metrics.png', save_kwargs={'bbox_inches': 'tight'})
-        probs, ground_truths = learn.get_preds(ds_idx=1)
-        predictions = np.argmax(probs, axis=1)
-        mlflow.log_figure(plot_confusion_matrix(ground_truths, predictions, learn.dls.vocab), 'confusion.png', save_kwargs={'bbox_inches': 'tight'})
+        try:
+            mlflow.log_figure(plot_losses(learn), 'losses.png', save_kwargs={'bbox_inches': 'tight'})
+            mlflow.log_figure(plot_metrics(learn, metrics_names), 'metrics.png', save_kwargs={'bbox_inches': 'tight'})
+            probs, ground_truths = learn.get_preds(ds_idx=1)
+            predictions = np.argmax(probs, axis=1)
+            mlflow.log_figure(plot_confusion_matrix(ground_truths, predictions, learn.dls.vocab), 'confusion.png', save_kwargs={'bbox_inches': 'tight'})
+        except Exception:
+            print('ERROR: the figures were not properly created or logged') 
 
         # Create and log tables
-        train_table = get_predictions_table(learn, learn.dls.train)
-        valid_table = get_predictions_table(learn, learn.dls.valid)
-        train_table.to_csv(f'{path}train_table.csv', index=False)
-        valid_table.to_csv(f'{path}valid_table.csv', index=False)
-        mlflow.log_artifact(f'{path}train_table.csv', '')
-        mlflow.log_artifact(f'{path}valid_table.csv', '')
+        try:
+            train_table = get_predictions_table(learn, learn.dls.train)
+            valid_table = get_predictions_table(learn, learn.dls.valid)
+            train_table.to_csv(f'{path}train_table.csv', index=False)
+            valid_table.to_csv(f'{path}valid_table.csv', index=False)
+            mlflow.log_artifact(f'{path}train_table.csv', '')
+            mlflow.log_artifact(f'{path}valid_table.csv', '')
+        except Exception:
+            print('ERROR: the tables were not properly created or logged') 
 
         # Log metrics
         results = learn.validate()
